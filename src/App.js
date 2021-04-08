3d784489c1673257fcb147964b31e504435e1380
jbus97 <hugofleitasbus@gmail.com>
2021-04-08 13:52:26 -0400

    Adds a file input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+
+function FileInput({ onChange }) {
+  return (
+    <label htmlFor="file-input">
+      Upload whatsapp chat
+      <br />
+      <input id="file-input" type="file" onChange={(e) => { onChange(e.target.files[0]); }} />
+    </label>
+  );
+}
+
+FileInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
 
 function App() {
+  const onChange = (file) => {
+    const fileReader = new FileReader();
+    // eslint-disable-next-line no-unused-vars
+    fileReader.onloadend = (e) => {
+      console.log(fileReader.result);
+    };
+    fileReader.readAsText(file);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FileInput onChange={onChange} />
     </div>
   );
 }
