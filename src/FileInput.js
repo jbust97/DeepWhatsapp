7174ac2c9a5bd14d363f23edaa489a6f3267e690
jbus97 <hugofleitasbus@gmail.com>
2021-06-19 13:05:09 -0400

    Hola

--- src/FileInput.js
+++ src/FileInput.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Grid,
   Button,
   Dialog,
+  Paper,
 } from '@material-ui/core';
 
 export default function FileInput({ onChange }) {
@@ -13,14 +15,29 @@ export default function FileInput({ onChange }) {
   return (
     <Grid item container spacing={0}>
       <Grid xs={12}>
-        <Button variant="contained" fullWidth onClick={() => { setOpen(true); }}>Nuevo Chat</Button>
+        <Button variant="contained" color="primary" fullWidth onClick={() => { setOpen(true); }}>Nuevo Chat</Button>
       </Grid>
       <Grid item xs={3}>
         <Dialog open={open} onClose={() => { setOpen(false); }}>
-          <input id="file-input" type="file" onChange={(e) => { onChange(e.target.files[0]); }} />
+          <Paper style={{
+            padding: '20px', maxWidth: '300px', minHeight: '200px', border: 'solid 2px red',
+          }}
+          >
+            <Grid container item style={{ boxSizing: 'border-box', marginTop: '50%', marginBottom: '50%' }}>
+              <Grid item xs={12}>
+                <div style={{ margin: 'auto' }}>
+                  <Button variant="contained" color="primary">
+                    <label htmlFor="file-input">
+                      Subir un chat de whatsapp
+                    </label>
+                    <input style={{ display: 'none' }} id="file-input" type="file" onChange={(e) => { onChange(e.target.files[0]); }} />
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Paper>
         </Dialog>
       </Grid>
-
     </Grid>
   );
 }
